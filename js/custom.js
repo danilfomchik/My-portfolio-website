6fbabbbba5de70334f9df4591929e2ea6044a846
d.fomenko <d.fomenko@viseven.com>
2022-02-08 18:18:48 +0200

    update 3d card and add switch arrow

--- js/custom.js
+++ js/custom.js
@@ -1,59 +1,74 @@
-const card3D = document.querySelector('.card-3d-container');
-const checkItem = document.querySelectorAll('.services__check-item');
+window.addEventListener('DOMContentLoaded', () => {
+    const card3D = document.querySelector('.card-3d-container');
+    const checkItem = document.querySelectorAll('.services__check-item');
 
-const cardFrontImg = document.querySelectorAll('.card-front__img');
-const cardBackImg = document.querySelectorAll('.card-back__img');
+    const cardFrontImg = document.querySelectorAll('.card-front__img');
+    const cardBackImg = document.querySelectorAll('.card-back__img');
 
-// find and deactivate elem
-const getActiveItem = () => document.querySelector('.active');
-const deactivateActiveItem = () => {
-    const active = getActiveItem();
-    if(active){
-        active.classList.remove('active')
-    }
-}
+    const arrow = document.querySelector('.arrow');
 
-// add acrive class
-function addActiveClass(arr, className) {
-    arr.forEach(img => {
-        img.classList.add(className);
-    });
-}
-
-// remove acrive class
-function removeActiveClass(arr, className) {
-    arr.forEach(img => {
-        img.classList.remove(className);
-    });
-}
-
-
-// adding classes for img
-function flipCard(clickingElem, checkedElem) {
-
-    if(clickingElem.dataset.item == checkedElem){
-
-        addActiveClass(cardFrontImg, 'passiveImg');
-        addActiveClass(cardBackImg, 'activeImg');
-    } else{
-
-        removeActiveClass(cardFrontImg, 'passiveImg')
-        removeActiveClass(cardBackImg, 'activeImg')
+    // find and deactivate elem
+    const getActiveItem = () => document.querySelector('.active');
+    const deactivateActiveItem = () => {
+        const active = getActiveItem();
+        if(active){
+            active.classList.remove('active')
+        }
     }
 
-}
+    // add acrive class
+    function addActiveClass(arr, className) {
+        arr.forEach(img => {
+            img.classList.add(className);
+        });
+    }
+
+    // remove acrive class
+    function removeActiveClass(arr, className) {
+        arr.forEach(img => {
+            img.classList.remove(className);
+        });
+    }
 
 
-// flip card by clicking 
-checkItem.forEach(item => {
+    // adding classes for img
+    function flipCard(clickingElem, checkedElem) {
 
-    item.addEventListener('click', (e) => {
-        deactivateActiveItem();
-        item.classList.add('active');
+        if(clickingElem.dataset.item == checkedElem){
 
-        card3D.classList.toggle('flip', item.dataset.item == 'camping');
-        flipCard(item, 'camping');
-        
+            addActiveClass(cardFrontImg, 'passiveImg');
+            addActiveClass(cardBackImg, 'activeImg');
+        } else{
+
+            removeActiveClass(cardFrontImg, 'passiveImg')
+            removeActiveClass(cardBackImg, 'activeImg')
+        }
+
+    }
+
+
+    // flip card by clicking 
+    checkItem.forEach(item => {
+
+        item.addEventListener('click', (e) => {
+            changeArrowDirection(e, arrow);
+
+            deactivateActiveItem();
+            item.classList.add('active');
+
+            card3D.classList.toggle('flip', item.dataset.item == 'coding');
+            flipCard(item, 'coding');
+            
+        });
+
     });
 
+    //change arrow direcrion
+    arrow.addEventListener('click', (e) => {
+        e.preventDefault();
+    });
+    function changeArrowDirection(e, elem) {
+        e.preventDefault();
+        elem.classList.toggle('arrow_active');
+    }
 });
