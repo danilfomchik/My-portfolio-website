1eb69f5f765238bf1f1856c0801ec7768afcea08
d.fomenko <d.fomenko@viseven.com>
2022-04-01 11:46:26 +0300

    fixed untracked files

--- src/js/modules/activeHeader.js
+++
@@ -1,7 +0,0 @@
-export function activeHeader() {
-    const header = document.querySelector('header');
-
-    window.addEventListener('scroll', () => {
-        header.classList.toggle('activeHeader', window.scrollY > 50);
-    });
-}
