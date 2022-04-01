30468dfe3c74f831a5134b0b9d85b47879fe4caa
d.fomenko <d.fomenko@viseven.com>
2022-04-01 11:13:28 +0300

    main

---
+++ src/js/modules/activeHeader.js
@@ -0,0 +1,7 @@
+export function activeHeader() {
+    const header = document.querySelector('header');
+
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('activeHeader', window.scrollY > 50);
+    });
+}
